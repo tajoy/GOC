e6ebb62cb210d62761c94563dbf2b79b7bbda6b9
tajoy <tj328111241@gmail.com>
2016-04-17 00:38:47 +0800

    修改UI,增加自定义模版参数列表 一点表格的Delegate代码

---
+++ TableInterface.py
@@ -0,0 +1,31 @@
+#!/bin/env python3
+# -*- coding: UTF-8 -*-
+
+
+from PyQt5 import QtWidgets, Qt
+
+class Delegate(QtWidgets.QStyledItemDelegate):
+    def __init__(self, parent=None):
+        QtWidgets.QStyledItemDelegate.__init__(self, parent)
+
+    def createEditor(self, parent, option, index):
+        #createEditor(self, QWidget, QStyleOptionViewItem, QModelIndex) -> QWidget
+        editor = QtWidgets.QSpinBox(parent)
+        editor.setFrame(False)
+        editor.setMinimum(0)
+        editor.setMaximum(100)
+        return editor
+
+    def setEditorData(self, editor, index):
+        #setEditorData(self, QWidget, QModelIndex)
+        value = index.model().data(index, Qt.ItemDataRole.EditRole).toInt()
+        spinBox = editor
+        spinBox.setValue(value)
+
+    def setModelData(self, editor, model, index):
+        #setModelData(self, QWidget, QAbstractItemModel, QModelIndex)
+        pass
+
+    def updateEditorGeometry(self, editor, option, index):
+        #updateEditorGeometry(self, QWidget, QStyleOptionViewItem, QModelIndex)
+        pass
